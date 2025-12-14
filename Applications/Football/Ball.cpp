e8216a109d562f4c4332984baf37b43b2adc3678
Fl0r3ntLem <folem0302@gmail.com>
2025-12-14 15:45:00 +0100

    collision between ball and players

--- Applications/Football/Ball.cpp
+++ Applications/Football/Ball.cpp
@@ -122,19 +122,24 @@ void Ball :: run() {
     const int FRAME_SKIP = 5; // only update movement once every 5 frames
     while (true) {
         if ((frame % FRAME_SKIP) == 0) {
-            if (isColliding(p1)) {
-                speed = acc;
-                counter_till_next_speed = speed;
+            {
+                Player* players[2] = {p1, p2};
+                for (Player* p : players) {
+                    if (isColliding(p)) {
+                        speed = acc;
+                        counter_till_next_speed = speed;
 
-                x_diff = x - p1->get_x();
-                y_diff = y - p1->get_y();
-                dx = (x_diff > 0) - (x_diff < 0);  // signum : -1, 0 ou 1
-                dy = (y_diff > 0) - (y_diff < 0);
-                if (x_diff < 0) x_diff = -x_diff;
-                if (y_diff < 0) y_diff = -y_diff;
-                err = x_diff - y_diff;
+                        x_diff = x - p->get_x();
+                        y_diff = y - p->get_y();
+                        dx = (x_diff > 0) - (x_diff < 0);  // signum : -1, 0 ou 1
+                        dy = (y_diff > 0) - (y_diff < 0);
+                        if (x_diff < 0) x_diff = -x_diff;
+                        if (y_diff < 0) y_diff = -y_diff;
+                        err = x_diff - y_diff;
 
-                // getOrientation();
+                        // getOrientation();
+                    }
+                }
             }
             move();
         }
